47bc22f83b07de6169515f59b0dbc617105e9057
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-02 12:56:09 -0400

    Build cache for source files working correctly

--- ng-borc.cpp
+++ ng-borc.cpp
@@ -245,8 +245,7 @@ public:
         this->cacheFile = cacheFile;
         this->loadCache();
 
-        std::fstream fsOutput {cacheFile.c_str(), std::ios_base::app};
-
+        fsOutput.open (cacheFile.c_str(), std::ios_base::out);
         if (! fsOutput.is_open()) {
             return;
         }
@@ -254,7 +253,7 @@ public:
 
 
     ~BuildCache() {
-        // this->saveCache();
+        this->saveCache();
     }
 
 
@@ -268,10 +267,21 @@ public:
 
 
     bool sourceNeedsRebuild(const std::string &sourceFile) const {
-        const time_t cachedTimestamp = this->getModifiedTime(sourceFile.c_str(), true).value();
-        const time_t currentTimestamp = this->getModifiedTime(sourceFile.c_str(), false).value();
+        std::cout << "Cheking " << sourceFile << std::endl;
 
-        return cachedTimestamp != currentTimestamp;
+        const auto cachedTimestamp = this->getModifiedTime(sourceFile.c_str(), true);
+        const auto currentTimestamp = this->getModifiedTime(sourceFile.c_str(), false);
+
+        if (!cachedTimestamp.has_value() || !currentTimestamp.has_value()) {
+            std::cout << "Must build (HARD): " << true << std::endl;
+            std::cout << "    cachedTimestamp: " << cachedTimestamp.has_value() << std::endl;
+            std::cout << "    currentTimestamp: " << currentTimestamp.has_value() << std::endl;
+
+            return true;
+        }
+
+        std::cout << "Must build: " << (cachedTimestamp.value() != currentTimestamp.value()) << std::endl;
+        return cachedTimestamp.value() != currentTimestamp.value();
     }
 
 private:
@@ -287,20 +297,36 @@ private:
         while (!fs.eof()) {
             std::getline(fs, line);
 
+            std::cout << "Parsing line: " << line << std::endl;
+
             size_t pos = line.find(':');
 
             if (pos == std::string::npos) {
+                std::cout << "    Separator not found: " << line << std::endl;
                 continue;
             }
 
             const std::string key = line.substr(0, pos);
-            const time_t value = static_cast<time_t>(std::atol(line.substr(pos, line.size()).c_str()));
+            const std::string value_str = line.substr(pos + 1, line.size());
+            const time_t value = static_cast<time_t>(std::atol(line.substr(pos + 1, line.size()).c_str()));
+            const long long_value = std::atol(line.substr(pos + 1, line.size()).c_str());
+
+            std::cout << "    key: " << key << std::endl;
+            std::cout << "    value_str: " << value_str << std::endl;
+            std::cout << "    value: " << value << std::endl;
+            std::cout << "    long_value: " << long_value << std::endl;
 
             sourceCache.insert({key, value});
         }
+
+        // 
+        std::cout << "Loaded cache: " << std::endl;
+        for (const auto &pair : sourceCache) {
+            std::cout << "    " << "\"" << pair.first << "\":" << pair.second << std::endl;
+        }
     }
 
-    /*
+    
     void saveCache() {
         std::fstream fs {cacheFile.c_str(), std::ios_base::out};
 
@@ -309,14 +335,16 @@ private:
         }
 
         for (const auto &pair : sourceCache) {
-            fs << "\"" << pair.first << "\":" << pair.second << std::endl;
+            fs << pair.first << ":" << pair.second << std::endl;
         }
     }
-    */
-
+    
 
     void appendEntryToCache(const std::string &sourceFile, const time_t modifiedTime) {
-        fsOutput << "\"" << sourceFile << "\":" << modifiedTime << std::endl;
+        // std::cout << "appendEntryToCache: " << sourceFile << ":" << modifiedTime << std::endl;
+
+        fsOutput  << sourceFile << ":" << modifiedTime << std::endl;
+        fsOutput.flush();
     }
 
 
